6840c35f0dddfbaeaae73a29165e4f9e309b0559
Juan Sebastian <jsbeleno@gmail.com>
2015-10-10 20:54:36 -0500

    Initial Commit

---
+++ app-cut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app-cut
+//
+//  Created by Juan Sebastián Beleño Díaz on 10/10/15.
+//  Copyright © 2015 Juan Sebastián Beleño Díaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app-cut/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  app-cut
+//
+//  Created by Juan Sebastián Beleño Díaz on 10/10/15.
+//  Copyright © 2015 Juan Sebastián Beleño Díaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
